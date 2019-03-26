730dc8078cf72086990add094432a4276b80f865
JangJongHwan <jangood1122@naver.com>
2019-03-26 17:09:09 +0900

    conv1d 복잡도 줄이고 (시작 4 filter), loss는 미싱만 복원으로

--- Conv_AE.py
+++ Conv_AE.py
@@ -7,7 +7,10 @@ class Conv_AE(nn.Module):
         super(Conv_AE ,self).__init__()
 
         self.Encoder = nn.Sequential(
-            nn.Conv1d(1 ,8 ,10 ,3),
+            nn.Conv1d(1 ,4 ,10 ,3),
+            nn.BatchNorm1d(4),
+            nn.Tanh(),
+            nn.Conv1d(4 ,8 ,10 ,2),
             nn.BatchNorm1d(8),
             nn.Tanh(),
             nn.Conv1d(8 ,16 ,10 ,2),
@@ -16,26 +19,23 @@ class Conv_AE(nn.Module):
             nn.Conv1d(16 ,32 ,10 ,2),
             nn.BatchNorm1d(32),
             nn.Tanh(),
-            nn.Conv1d(32 ,64 ,10 ,2),
-            nn.BatchNorm1d(64),
-            nn.Tanh(),
-            nn.Conv1d(64 ,128 ,10 ,2)
+            nn.Conv1d(32 ,64 ,10 ,2)
         )
 
         self.Decoder = nn.Sequential(
-            nn.ConvTranspose1d(128 ,64 ,10 ,1),
-            nn.BatchNorm1d(64),
-            nn.Tanh(),
             nn.ConvTranspose1d(64 ,32 ,10 ,1),
             nn.BatchNorm1d(32),
             nn.Tanh(),
             nn.ConvTranspose1d(32 ,16 ,10 ,1),
             nn.BatchNorm1d(16),
             nn.Tanh(),
-            nn.ConvTranspose1d(16 ,8 ,10 ,4 ,output_padding=1),
+            nn.ConvTranspose1d(16 ,8 ,10 ,1),
             nn.BatchNorm1d(8),
             nn.Tanh(),
-            nn.ConvTranspose1d(8 ,1 ,10 ,5),
+            nn.ConvTranspose1d(8 ,4 ,10 ,4 ,output_padding=1),
+            nn.BatchNorm1d(4),
+            nn.Tanh(),
+            nn.ConvTranspose1d(4 ,1 ,10 ,5),
             nn.Tanh()
         )
 
